38dcc2fbd5648d700a819bf84949462e57941784
Zihad <mojahidulislamzihad686@gmail.com>
2026-07-15 23:15:25 +0600

    Add files via upload

---
+++ student marks.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    printf("enter your marks :");
+    scanf("%d",&a);
+    if (a<=100 && a>=0)
+    {
+        if (a<=100 && a>=80)
+        {
+            printf("A+");
+        }
+        else if (a<=79 && a>=70)
+        {
+            printf("A");
+        }
+        else if (a<=69 && a>=60)
+        {
+            printf("A-");
+        }
+        else if (a<=59 && a>=50)
+        {
+            printf("B");
+        }
+        else if (a<=49 && a>=40)
+        {
+            printf("C");
+        }
+        else if (a<=39 && a>=33)
+        {
+            printf("D");
+        }
+        else
+        {
+            printf("F");
+        }
+    }
+    else
+    {
+        printf("Wrong Number ");
+    }
+    return 0;
+
+}
